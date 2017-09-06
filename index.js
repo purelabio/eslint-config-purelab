bf03af5044b1559b2edef85ac061b30cbb6342fc
Nikita Aristov <nikita.aristov@gmail.com>
2017-09-06 20:08:54 +0300

    react and jsx modules and another cosmetics

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 module.exports = {
   extends: [
-    './rules/main',
+    "./rules/main",
+    "./rules/react",
   ].map(require.resolve),
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: "module",
     ecmaFeatures: {
+      jsx: true,
       experimentalObjectRestSpread: true,
     },
   },
+  plugins: [
+    "react",
+    "jsx-a11y",
+  ],
   rules: {
-    strict: 'error',
+    strict: "error",
   },
 }
---
+++ rules/react.js
@@ -0,0 +1,27 @@
+module.exports = {
+  rules: {
+    "jsx-a11y/aria-props": "error",
+    "jsx-a11y/heading-has-content": "off",
+    "jsx-a11y/href-no-hash": "error",
+    // "jsx-a11y/label-has-for": "error",
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/mouse-events-have-key-events": "error",
+    "jsx-a11y/role-has-required-aria-props": "error",
+    "jsx-a11y/role-supports-aria-props": "error",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-quotes": "off",
+
+    "react/forbid-prop-types": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/jsx-first-prop-new-line": ["error", "multiline"],
+    "react/jsx-filename-extension": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-no-bind": "off",
+    "react/jsx-no-target-blank": "off",
+    // "react/no-string-refs": "off",
+    "react/prefer-stateless-function": "off",
+    "react/prop-types": "off",
+    "react/require-extension": "off",
+    "react/self-closing-comp": "off",
+  },
+}
