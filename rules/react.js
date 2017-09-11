31e325c2901e3032d8a2ca513e587383da9bcddf
Nikita Aristov <nikita.aristov@gmail.com>
2017-09-11 19:23:29 +0300

    fixed property for jsx anchors

--- rules/react.js
+++ rules/react.js
@@ -2,15 +2,15 @@
 
 module.exports = {
   rules: {
+    "jsx-a11y/anchor-is-valid": "error",
     "jsx-a11y/aria-props": "error",
     "jsx-a11y/heading-has-content": "off",
-    "jsx-a11y/href-no-hash": "error",
-    // "jsx-a11y/label-has-for": "error",
     "jsx-a11y/label-has-for": "off",
     "jsx-a11y/mouse-events-have-key-events": "error",
     "jsx-a11y/role-has-required-aria-props": "error",
     "jsx-a11y/role-supports-aria-props": "error",
     "jsx-a11y/no-static-element-interactions": "off",
+
     "jsx-quotes": "off",
 
     "react/forbid-prop-types": "off",
@@ -20,7 +20,6 @@ module.exports = {
     "react/jsx-indent": "off",
     "react/jsx-no-bind": "off",
     "react/jsx-no-target-blank": "off",
-    // "react/no-string-refs": "off",
     "react/jsx-uses-vars": "error",
     "react/prefer-stateless-function": "off",
     "react/prop-types": "off",
